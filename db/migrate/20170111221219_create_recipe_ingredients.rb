1f7e4153923460cf28bc73cf77b18d0cf8dbd4ad
ayafushimi <aya.fushimi@gmail.com>
2017-01-11 17:14:33 -0500

    generated model and migration files for RecipeIngredient

---
+++ db/migrate/20170111221219_create_recipe_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateRecipeIngredients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipe_ingredients do |t|
+      t.belongs_to :recipe, foreign_key: true
+      t.belongs_to :ingredient, foreign_key: true
+      t.integer :quantity
+      t.string :unit
+
+      t.timestamps
+    end
+  end
+end
