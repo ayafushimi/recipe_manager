f289054f8e0f7fc3654ef905ea0e479f6e8490fb
ayafushimi <aya.fushimi@gmail.com>
2017-01-11 17:01:27 -0500

    generated contoller, veiw and helper files for Recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipe/index'
+
+  get 'recipe/new'
+
+  get 'recipe/show'
+
+  get 'recipe/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
