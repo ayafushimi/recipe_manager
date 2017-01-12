d3ae6e208a5c3896ccae5b846c399ba9d22969a1
ayafushimi <aya.fushimi@gmail.com>
2017-01-12 12:31:59 -0500

    difined routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :reports, shallow: true
   end
+  resource :users, only: [:create, :show, :edit, :update, :destroy]
+  get 'signup', to: 'users#new', as: :signup
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
