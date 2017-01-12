6cd162f7f72bd26298a82281aea89544e3f3262c
ayafushimi <aya.fushimi@gmail.com>
2017-01-12 12:34:22 -0500

    difined routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root 'recipes#index'
+
   resources :recipes do
     resources :reports, shallow: true
   end
+
   resource :users, only: [:create, :show, :edit, :update, :destroy]
   get 'signup', to: 'users#new', as: :signup
+
+  resource :sessions, only: [:create]
+  get 'signin', to: 'sessions#new', as: :signin
+  get 'signout', to: 'sessions#destroy', as: :signout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
