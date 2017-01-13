6c4dea920e0e05353cd48752b0f0bd9c0c5ea7ad
ayafushimi <aya.fushimi@gmail.com>
2017-01-13 12:40:28 -0500

    deleted accept_nested_attributes for ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,5 +5,5 @@ class Recipe < ApplicationRecord
   has_many :reports
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
-  accepts_nested_attributes_for :recipe_ingredients, :ingredients, :directions
+  accepts_nested_attributes_for :recipe_ingredients, :directions
 end
