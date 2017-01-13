24d305d071bbb50b48aee4c2d0b58e97fb9be46d
ayafushimi <aya.fushimi@gmail.com>
2017-01-13 09:05:19 -0500

    addedd foreign_key to Recipe's creator

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :directions
   has_many :reports
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
   accepts_nested_attributes_for :recipe_ingredients, :ingredients, :directions
 end
