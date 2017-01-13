aad6f7b3d8c81f657dd6c3c9a4efcd8c6d5c0beb
ayafushimi <aya.fushimi@gmail.com>
2017-01-13 14:04:48 -0500

    defined ingredient_attributes=

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,4 +3,8 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
 
   accepts_nested_attributes_for :ingredient
+
+  def ingredient_attributes=(attributes)
+    self.ingredient = Ingredient.find_or_create_by(attributes)
+  end
 end
