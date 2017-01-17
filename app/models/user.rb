c6e30a6fe83d731451cda9063cfdc4dc941bf7a4
ayafushimi <aya.fushimi@gmail.com>
2017-01-17 12:16:09 -0500

    added validates to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :reports
   has_many :recipes
   has_secure_password
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password, length: {in: 6..30}
 end
