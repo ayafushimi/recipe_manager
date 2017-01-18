4dd719ef2b097e009feef1babd570dc8c3cf08c5
ayafushimi <aya.fushimi@gmail.com>
2017-01-17 19:30:09 -0500

    defined .has_no_report

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -23,6 +23,10 @@ class Recipe < ApplicationRecord
     self.where(id: Report.pluck(:recipe_id).uniq)
   end
 
+  def self.has_no_report
+    self.where.not(id: Report.pluck(:recipe_id).uniq)
+  end
+
   def self.order_by_rate
     self.joins(:reports).select("'recipes'.'id', 'recipes'.'user_id', 'recipes'.'title', 'recipes'.'time', 'recipes'.'link','reports'.'recipe_id', AVG('reports'.'rate')").group("'reports'.'recipe_id'").order("AVG('reports'.'rate') DESC")
   end
