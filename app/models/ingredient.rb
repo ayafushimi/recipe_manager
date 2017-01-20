dbf5cbbfc91d6455a3ff353e35bde90eb46d9ca2
ayafushimi <aya.fushimi@gmail.com>
2017-01-20 18:26:26 -0500

    added validation to Ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  validates :title, uniqueness: { case_sensitive: false }
 end
