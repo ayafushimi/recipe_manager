005b6654dccf2a8e8a6baa49b9233147cce37351
ayafushimi <aya.fushimi@gmail.com>
2017-01-23 14:57:46 -0500

    added sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(
+      username: session_params[:username],
+      email: session_params[:email]
+    ).try(:authenticate, session_params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session)
   end
 end
