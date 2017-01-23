c64100a9d89c6d233f9a7df413f2074b366637c4
ayafushimi <aya.fushimi@gmail.com>
2017-01-23 15:01:15 -0500

    added sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_url
+  end
+
   private
 
   def session_params
