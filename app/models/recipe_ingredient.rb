b4d4ce37ddcf93a664a8fbcd3bf7f2e1a5b5feaf
ayafushimi <aya.fushimi@gmail.com>
2017-01-24 18:57:43 -0500

    added :optional to belongs_to recipe

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,10 +1,10 @@
 class RecipeIngredient < ApplicationRecord
-  belongs_to :recipe
+  belongs_to :recipe, optional: true
   belongs_to :ingredient
 
   accepts_nested_attributes_for :ingredient
 
   def ingredient_attributes=(attributes)
-    self.ingredient = Ingredient.find_or_create_by(attributes)
+    self.ingredient = Ingredient.find_or_create_by(title: attributes["title"].downcase)
   end
 end
