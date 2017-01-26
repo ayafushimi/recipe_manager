17c33106fb9483845d8be21668d1ff1b4662ee17
ayafushimi <aya.fushimi@gmail.com>
2017-01-26 13:14:21 -0500

    added report/edit.html, report#edit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,6 +9,23 @@ class ReportsController < ApplicationController
     @report = @recipe.reports.build
   end
 
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    report = recipe.reports.build(report_params)
+    if report.save
+      redirect_to recipe_path(recipe)
+    else
+      render :new
+    end
+  end
+
   def edit
+    @report =  Report.find(params[:id])
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:id, :rate, :comment, :user_id)
   end
 end
