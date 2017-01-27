6d6bb8dc6e842a3109fb6c3f888c231390a63a8d
ayafushimi <aya.fushimi@gmail.com>
2017-01-27 12:15:30 -0500

    modified user#create, user#update to hand over @user to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render :new
     end
@@ -24,9 +24,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
-      redirect_to user_path(user)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
     else
       render :edit
     end
