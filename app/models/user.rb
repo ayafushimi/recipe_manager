314a802bd0d8debdf98cd3a8a24549e09952b012
ayafushimi <aya.fushimi@gmail.com>
2017-01-27 12:00:20 -0500

    added before_destroy :delete_children to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
 
   validates :username, :email, presence: true, uniqueness: true
   validates :password, length: {in: 6..30}
+
+  before_destroy :delete_children
+
+  def delete_children
+    self.recipes.each {|recipe| recipe.destroy}
+    self.reports.each {|report| report.destroy}
+  end
 end
