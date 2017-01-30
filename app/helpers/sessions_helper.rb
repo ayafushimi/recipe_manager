705a4c6050a52f924b5d7831049eaa29c39d6cdf
ayafushimi <aya.fushimi@gmail.com>
2017-01-30 16:30:06 -0500

    moved need_login method to sessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,12 @@ module SessionsHelper
   def current_user
     User.find(session[:user_id])
   end
+
+  def need_login
+    unless logged_in?
+      flash[:danger] = "Please sign in to access that page."
+      redirect_to signin_path
+    end
+  end
+
 end
