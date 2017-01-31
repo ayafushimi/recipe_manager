821dd0339ed741206a3abcfc79ad0cd036fdf17c
ayafushimi <aya.fushimi@gmail.com>
2017-01-31 15:09:03 -0500

    added attributes

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,19 @@
 class Search < ApplicationRecord
+  ATTRIBUTES = %i(
+    order_by_rate
+    order_by_time
+    has_report
+    has_no_report
+    rate_over
+    rate_over
+    time_over
+    time_over
+    time_over
+    by_ingredient
+    has_report_by
+  )
+  attr_accessor(*ATTRIBUTES)
+
   def self.filter
     result = Recipe.all
     result = result.order_by_time
