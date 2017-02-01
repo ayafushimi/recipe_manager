1c68008ce26a1d79fa4fb4514c09dd9b5ed3d67f
ayafushimi <aya.fushimi@gmail.com>
2017-02-01 13:21:50 -0500

    defined has_recipe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,8 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
 
   validates :title, uniqueness: { case_sensitive: false }
+
+  def self.has_recipe
+    self.joins(:recipe_ingredients).merge(RecipeIngredient.where.not(recipe_id: nil)).distinct
+  end
 end
